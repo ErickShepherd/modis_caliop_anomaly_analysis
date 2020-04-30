56c64293cdc2aabbcb142ffcd481fd69ac26b063
olivian13 <49542531+olivian13@users.noreply.github.com>
2020-04-29 21:34:05 -0400

    Update placingDataInGrid.py

--- sea_ice_analysis/placingDataInGrid.py
+++ sea_ice_analysis/placingDataInGrid.py
@@ -4,38 +4,47 @@ import numpy as np
 latArray = np.arange(-90,91)
 lonArray = np.arange(0,360)
 
-lat = -89.1
-lon = 3.2
-conc = 4
-lat_array_temp = latArray
-lat_array_temp = np.append(lat_array_temp, lat)
-lat_array_temp.sort()
+lat = [[-89.1, -87, -87, -89.3], [-89.1, -87, -87, -89.3]]
+lon = [[3.2, 1.7, 3.1, 3.5],[3.2, 1.7, 3.1, 3.5]]
+conc = [[4, 97, 45, 100],[4, 97, 45, 100]]
 
-lon_array_temp = lonArray
-lon_array_temp = np.append(lon_array_temp, lon)
-lon_array_temp.sort()
+grid=np.zeros((4,4,2))
 
-latPos = np.where(lat_array_temp == lat)
-lonPos = np.where(lon_array_temp == lon)
-
-# create 3D array; rows are lat, cols are long
-# and inner most array has format of:
-#[sum of probabilities of there being ice for each data point in the grid, num of data points in the grid] 
-grid=np.zeros((180,360,2))
-
-# if concentration >= 15% then there is ice present
-if conc >= 15:
-    icePresent = True    
-else:
-    icePresent = False
-
-if grid[latPos[0][0] - 1][lonPos[0][0] - 1][1] == 0:
-    count = 1
-    grid[latPos[0][0] - 1][lonPos[0][0] - 1][0] = icePresent
-    grid[latPos[0][0] - 1][lonPos[0][0] - 1][1] = count
+for i in range(len(lat)):
+    for j in range(len(lat[i])):
+        lat_array_temp = latArray
+        lat_array_temp = np.append(lat_array_temp, lat[i][j])
+        lat_array_temp.sort()
     
-else:
-    grid[latPos[0][0] - 1][lonPos[0][0] - 1][0] += icePresent
-    grid[latPos[0][0] - 1][lonPos[0][0] - 1][1] += 1
+        lon_array_temp = lonArray
+        lon_array_temp = np.append(lon_array_temp, lon[i][j])
+        lon_array_temp.sort()
+        
+        latPos = np.where(lat_array_temp == lat[i][j])
+        lonPos = np.where(lon_array_temp == lon[i][j])
 
+        if conc[i][j] >= 15:
+            icePresent = True
+        else:
+            icePresent = False
+
+        if grid[latPos[0][0] - 1][lonPos[0][0] - 1][1] == 0:
+
+            count = 1
+            grid[latPos[0][0] - 1][lonPos[0][0] - 1][0] = icePresent
+            grid[latPos[0][0] - 1][lonPos[0][0] - 1][1] = count
+        
+        else:
+
+            grid[latPos[0][0] - 1][lonPos[0][0] - 1][0] += icePresent
+            grid[latPos[0][0] - 1][lonPos[0][0] - 1][1] += 1
+
+
+concProb = np.zeros((4,4))
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        concProb[i][j] = grid[i][j][0]/grid[i][j][1]
+            
 print(grid)
+print(concProb)
